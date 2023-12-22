75dc0cc8b4898b0c83c332c82b4294f8b6f4ae7d
opamao <theodoreyapi@gmail.com>
2023-12-22 17:50:48 +0000

    suite ajout video

--- lib/screens/splashs/screens/intro_screen.dart
+++ lib/screens/splashs/screens/intro_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:sizer/sizer.dart';
+import 'package:video_player/video_player.dart';
 
 import '../../../constants/constants.dart';
 import '../../../themes/themes.dart';
@@ -18,64 +20,87 @@ class IntroScreen extends StatefulWidget {
 }
 
 class _IntroScreenState extends State<IntroScreen> {
+
+  final VideoPlayerController _controller =
+  VideoPlayerController.asset("assets/videos/video.mp4");
+  ChewieController? chewieController;
+
+  @override
+  void initState() {
+    chewieController = ChewieController(
+        videoPlayerController: _controller,
+        autoPlay: true,
+        looping: true,
+        showControls: false,
+        allowFullScreen: true,
+        allowMuting: true,
+        allowPlaybackSpeedChanging: false,
+        aspectRatio: 9 / 20);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    chewieController!.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/min.png"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(4.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              "Le service de livraison a l'ivoirienne",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: colorWhite,
-                fontWeight: FontWeight.bold,
-                fontSize: 13.sp,
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Chewie(controller: chewieController!),
+        Padding(
+          padding: EdgeInsets.all(4.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Le service de livraison a l'ivoirienne",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: colorWhite,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 13.sp,
+                ),
               ),
-            ),
-            Image.asset("assets/images/rond.png", height: 25.h,),
-            Column(
-              children: [
-                SubmitButton(
-                  Constants.btnLogin,
-                  textcouleur: colorWhite,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ),
-                    );
-                  },
-                ),
-                Gap(2.h),
-                SubmitButton(
-                  Constants.btnRegister,
-                  textcouleur: colorWhite,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const RegisterScreen(),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
+              Image.asset("assets/images/rond.png", height: 25.h,),
+              Column(
+                children: [
+                  SubmitButton(
+                    Constants.btnLogin,
+                    textcouleur: colorWhite,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                  Gap(2.h),
+                  SubmitButton(
+                    Constants.btnRegister,
+                    textcouleur: colorWhite,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RegisterScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
